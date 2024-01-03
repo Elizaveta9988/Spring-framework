94a61814336d155bec23c639f2d947ffe3c35473
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2024-01-03 15:28:48 +0300

    New Documet

---
+++ Client.java
@@ -0,0 +1,19 @@
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("127.0.0.1", 8000);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            System.out.println("connected to server");
+            String request ="Murmansk-321321";
+            System.out.println("Request: "+request);
+            writer.write(request+'\n');
+            writer.flush();
+
+            String response = reader.readLine();
+            System.out.println("Response: "+response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
